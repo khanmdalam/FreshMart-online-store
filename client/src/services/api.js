641f9d534d0490ff3a062706048a5c0670b235b9
khanmdalam <nooralam1342004@gmail.com>
2026-05-07 02:03:37 +0530

    fronted

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 const API = axios.create({
-    //baseURL: 'https://freshmart-0the.onrender.com/api'
-    baseURL: 'http://localhost:5000/api'
+    baseURL: 'https://freshmart-online-store.onrender.com/api'
+   // baseURL: 'http://localhost:5000/api'
     
 })
 
